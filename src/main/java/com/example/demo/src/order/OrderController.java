006379206edda77d71e1e8e2e992276a071bbd6e
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-07-29 10:25:59 +0900

    {2022-07-29} 주문내역조회api

--- src/main/java/com/example/demo/src/order/OrderController.java
+++ src/main/java/com/example/demo/src/order/OrderController.java
@@ -2,6 +2,7 @@ package com.example.demo.src.order;
 
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
+import com.example.demo.src.order.model.GetOrderRes;
 import com.example.demo.src.order.model.PostOrderReq;
 import com.example.demo.src.order.model.PostOrderRes;
 import org.slf4j.Logger;
@@ -26,13 +27,23 @@ public class OrderController {
     @ResponseBody
     @PostMapping("/{userIdx}")
     public BaseResponse<List<PostOrderRes>> doOrder(@PathVariable("userIdx") int userIdx, @RequestParam(value = "basketId") List<Integer> basketId,@RequestBody PostOrderReq postOrderReq) {
-        System.out.println(userIdx);
-        System.out.println(basketId);
         try{
             List<PostOrderRes> postOrderRes = orderService.doOrder(userIdx,basketId,postOrderReq);
             return new BaseResponse<>(postOrderRes);
         } catch(BaseException exception){
             return new BaseResponse<>((exception.getStatus()));
         }
+    }
+
+    @ResponseBody
+    @GetMapping("/{userIdx}")
+    public BaseResponse<List<GetOrderRes>> getHistory(@PathVariable("userIdx") int userIdx){
+        try{
+            List<GetOrderRes> getOrderRes = orderProvider.getHistory(userIdx);
+            return new BaseResponse<>(getOrderRes);
+        }catch (BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
     }
 }
--- src/main/java/com/example/demo/src/order/OrderDao.java
+++ src/main/java/com/example/demo/src/order/OrderDao.java
@@ -1,8 +1,10 @@
 package com.example.demo.src.order;
 
+import com.example.demo.src.order.model.GetOrderRes;
 import com.example.demo.src.order.model.PostOrderReq;
 import com.example.demo.src.order.model.PostOrderRes;
 import com.example.demo.src.product.model.GetSizeRes;
+import com.example.demo.src.product.model.GetTopSizeRes;
 import com.example.demo.src.shipment.model.PostShipmentReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -42,6 +44,27 @@ public class OrderDao {
                         rs.getInt("BASKET_ID"),
                         rs.getLong("BUY_ID"),
                         rs.getString("REQUEST")),
+                userIdx);
+    }
+    public List<GetOrderRes> getHistory(int userIdx){
+        String Query = "select BRAND_NAME,PRODUCT_NAME,ps.SIZE_TYPE,DATE(o.CREATED_AT) as ORDERED_DAY,o.BUY_ID,p.PRODUCT_PRICE*b.PRODUCT_QUANTITY as TOTAL,b.PRODUCT_QUANTITY\n" +
+                "from `ORDER` o\n" +
+                "inner join BASKET b on o.BASKET_ID = b.BASKET_ID\n" +
+                "inner join USER u on u.USER_ID = b.USER_ID\n" +
+                "inner join PRODUCT p on b.PRODUCT_ID = p.PRODUCT_ID\n" +
+                "inner join PRODUCT_SIZE ps on b.SIZE_ID = ps.SIZE_ID\n" +
+                "inner join BRAND b2 on p.BRAND_ID = b2.BRAND_ID\n" +
+                "inner join STOCK s on b.PRODUCT_ID = s.PRODUCT_ID and b.SIZE_ID=s.SIZE_ID and p.PRODUCT_ID=s.PRODUCT_ID\n" +
+                "where u.USER_ID=?;";
+        return this.jdbcTemplate.query(Query,
+                (rs, rowNum) -> new GetOrderRes(
+                        rs.getString("BRAND_NAME"),
+                        rs.getString("PRODUCT_NAME"),
+                        rs.getString("SIZE_TYPE"),
+                        rs.getDate("ORDERED_DAY"),
+                        rs.getLong("BUY_ID"),
+                        rs.getInt("TOTAL"),
+                        rs.getInt("PRODUCT_QUANTITY")),
                 userIdx);
     }
 }
