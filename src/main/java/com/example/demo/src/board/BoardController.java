5c96bc97250c60202b345cc8eab0cfefe401da5e
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-11-25 15:17:27 +0900

    {2022.11.24} * 과제평가 GET 파라미터 @Nullable

--- src/main/java/com/example/demo/src/board/BoardController.java
+++ src/main/java/com/example/demo/src/board/BoardController.java
@@ -7,6 +7,7 @@ import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -117,7 +118,7 @@ public class BoardController {
      */
     @ResponseBody
     @GetMapping("/evaluation-subjects") // (GET) www.seop.site/app/board/evaluation-subjects
-    public BaseResponse<List<GetEvaluateSubjectRes>> getEvaluateSubjectList(@RequestParam Integer grade){
+    public BaseResponse<List<GetEvaluateSubjectRes>> getEvaluateSubjectList(@RequestParam @Nullable Integer grade){
         try{
             List<GetEvaluateSubjectRes> getEvaluateSubjectRes = boardProvider.getEvaluateSubjectList(grade);
             return new BaseResponse<>(getEvaluateSubjectRes);
